e5b4fec1d27a0fb04f984103ebae1ef5b59dd350
Aida Sagadat <aidasagadat355@gmail.com>
2024-03-01 22:13:25 +0600

    smth

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,3 @@
 package com.example.demo.controller;
 
-@RestController
-@RequestMapping
-public class UserController {
-    @GetMapping
-    public String sayHi(){
-        return "Hi";
-    }
-}
+
