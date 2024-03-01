ad2667378db6839e17ea2098d55fa1d3cfcc17d9
Amina <231599@astanait.edu.kz>
2024-03-02 00:54:06 +0600

    First init

--- src/main/java/NeogurRepository.java
+++ src/main/java/NeogurRepository.java
@@ -1,7 +1,11 @@
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
+
+
+
 public interface NeogurRepository extends JpaRepository<Neogur, Integer> {
     // Additional custom queries if needed
 }
