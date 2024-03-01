db4f79c6465d35bb855a56f2d82d4026a66d5189
Amina <231599@astanait.edu.kz>
2024-03-01 23:02:44 +0600

    First init

---
+++ src/main/java/Buldak.java
@@ -0,0 +1,32 @@
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@Table("Buldak")
+public class Buldak extends Ramen {
+
+    public Buldak(int id, String name, int price) {
+        super(id, name, price);
+    }
+
+    public void add() {
+        // Assuming BuldakRepository is autowired or injected by Spring
+        BuldakRepository buldakRepository = /* inject this using Spring */ null;
+        buldakRepository.save(this);
+    }
+
+    public void update() {
+        // Assuming BuldakRepository is autowired or injected by Spring
+        BuldakRepository buldakRepository = /* inject this using Spring */ null;
+        buldakRepository.save(this);
+    }
+}
+
+interface BuldakRepository extends CrudRepository<Buldak, Integer> {
+    // Custom queries or methods can be added here if needed
+}
