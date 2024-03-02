8dd8e9072f2d60c82e17b1c380623b61a949805e
Amina <231599@astanait.edu.kz>
2024-03-02 09:13:02 +0600

    First init

---
+++ src/main/java/com/example/demo/repositories/SamyangRepository.java
@@ -0,0 +1,30 @@
+package com.example.demo.repositories;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@Table("Seafood")
+public class Seafood extends Ramen {
+
+    public Seafood(int id, String name, int price) {
+        super(id, name, price);
+    }
+
+    public void add() {
+        // Assuming SeafoodRepository is autowired or injected by Spring
+        SeafoodRepository seafoodRepository = /* inject this using Spring */ null;
+        seafoodRepository.save(this);
+    }
+
+    public void update() {
+        // Assuming SeafoodRepository is autowired or injected by Spring
+        SeafoodRepository seafoodRepository = /* inject this using Spring */ null;
+        seafoodRepository.save(this);
+    }
+}
